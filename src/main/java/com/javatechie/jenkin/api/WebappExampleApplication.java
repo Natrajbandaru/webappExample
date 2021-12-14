f96c4c6c31eaac0dbcdffd81340a352de9f4d213
Natrajbandaru <natrajbandaru99@gmail.com>
2021-12-14 16:56:14 +0530

    initialcommit

---
+++ src/main/java/com/javatechie/jenkin/api/WebappExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.jenkin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javatechie/jenkin/api/cantroller/Cantroller.java
@@ -0,0 +1,17 @@
+package com.javatechie.jenkin.api.cantroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Cantroller {
+	
+	@GetMapping("webappExample")
+	public String app() {
+		
+		return "welcome to KK tutorials";
+	}
+	
+	
+
+}
