af79419494041d5a17e84fc5742caa6286737831
Natrajbandaru <natrajbandaru99@gmail.com>
2021-12-14 17:21:44 +0530

    initialcommit

--- src/main/java/com/javatechie/jenkin/api/cantroller/Cantroller.java
+++ src/main/java/com/javatechie/jenkin/api/cantroller/Cantroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Cantroller {
 	
-	@GetMapping("webappExample")
+	@GetMapping("webappExample/")
 	public String app() {
 		
 		return "welcome to KK tutorials";
